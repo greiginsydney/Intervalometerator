478950e2dbd31632062e53e69c07c6d8df61e016
Greig Sheridan <greiginsydney@users.noreply.github.com>
2018-11-24 08:46:08 +1100

    Create wsgi.py

---
+++ Raspberry Pi/wsgi.py
@@ -0,0 +1,20 @@
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+# as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program.  If not, see 
+# <http://www.gnu.org/licenses/>.
+#
+# This script is part of the Intervalometerator project, a time-lapse camera controller for DSLRs:
+# https://github.com/greiginsydney/Intervalometerator
+# https://greiginsydney.com/Intervalometerator
+# https://intvlm8r.com
+
+# Reference: https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-gunicorn-and-nginx-on-ubuntu-16-04
+
+from intvlm8r import app
+
+if __name__ == "__main__":
+    app.run()
