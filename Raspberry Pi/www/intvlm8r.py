1a590982a69832c72d49554a00a1cccdc14de687
Greig Sheridan <greiginsydney@users.noreply.github.com>
2019-07-10 20:46:59 +1000

    Initialised 'thumbscount' in ini file.

--- Raspberry Pi/www/intvlm8r.py
+++ Raspberry Pi/www/intvlm8r.py
@@ -1086,6 +1086,7 @@ def createConfigFile(iniFile):
         config = ConfigParser.ConfigParser()
         config.add_section('Global')
         config.set('Global', 'file created', time.strftime("%0d %b %Y",time.localtime(time.time())))
+        config.set('Global', 'thumbscount', 20)
         config.add_section('Transfer')
         config.set('Transfer', 'tfrMethod', 'manual')
         config.add_section('Copy')
