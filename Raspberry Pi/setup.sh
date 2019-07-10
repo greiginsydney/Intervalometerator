ed6342ea4252012c7fdad860b99da14ff8e08ab0
Greig Sheridan <greiginsydney@users.noreply.github.com>
2019-07-10 14:00:07 +1000

    Added copyright header

--- Raspberry Pi/setup.sh
+++ Raspberry Pi/setup.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# This script is part of the Intervalometerator project, a time-lapse camera controller for DSLRs:
+# https://github.com/greiginsydney/Intervalometerator
+# https://greiginsydney.com/intvlm8r
+# https://intvlm8r.com
+
+
 set -e # The -e switch will cause the script to exit should any command return a non-zero value
 
 # keep track of the last executed command
