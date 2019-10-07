3e475ac32fce4b63e6f46d0915110bb65414343e
Greig Sheridan <greiginsydney@users.noreply.github.com>
2019-10-07 20:05:10 +1100

    Overhauled the ap/noap functions
    'ap':
    - the menu wasn't correctly pulling existing DHCP values from /etc/dnsmasq.conf
    
    'noap':
    - now asks if you want a static or dynamic IP as its outcome.
    - if you request static, a menu takes you through the options. 
    - the menu pre-populates from existing values (like the 'ap' menu)

--- Raspberry Pi/setup.sh
+++ Raspberry Pi/setup.sh
@@ -245,6 +245,16 @@ IPprefix_by_netmask ()
 }
 
 
+# https://stackoverflow.com/questions/20762575/explanation-of-convertor-of-cidr-to-netmask-in-linux-shell-netmask2cdir-and-cdir
+CIDRtoNetmask ()
+{
+	# Number of args to shift, 255..255, first non-255 byte, zeroes
+	set -- $(( 5 - ($1 / 8) )) 255 255 255 255 $(( (255 << (8 - ($1 % 8))) & 255 )) 0 0 0
+	[ $1 -gt 1 ] && shift $1 || shift
+	echo ${1-0}.${2-0}.${3-0}.${4-0}
+}
+
+
 make_ap ()
 {
 	apt-get install dnsmasq hostapd -y
@@ -253,11 +263,14 @@ make_ap ()
 	sed -i -E "s|^#?(DAEMON_CONF=\")(.*)\"|\1/etc/hostapd/hostapd.conf\"|" /etc/default/hostapd
 	if  grep -Fq "interface wlan0" "/etc/dhcpcd.conf";
 	then
+		wlanLine=$(sed -n '/interface wlan0/=' /etc/dhcpcd.conf) #This is the line number that the wlan config starts at
 		#Un-comment the lines if they're present but inactive:
-		sed -i -E "s/^#(interface wlan0\s*)/\1/" /etc/dhcpcd.conf
+		sed -i -E "s/^#(interface wlan0.*)/\1/" /etc/dhcpcd.conf
 		# https://unix.stackexchange.com/questions/285160/how-to-edit-next-line-after-pattern-using-sed
-		sed -i -E '$!N;s/(wlan0\n)#(\s*static)/\1\2/;P;D' /etc/dhcpcd.conf # Replaces only if "static" is on the line AFTER "wlan0"
-		sed -i -E "s/^#(\s*nohook wpa_supplicant.*)/\1/" /etc/dhcpcd.conf
+		sed -i -E '$!N;s/(wlan0\n)#(\s*static\s*ip_address)/\1\2/;P;D' /etc/dhcpcd.conf # Replaces only if "static ip_address" is on the line AFTER "wlan0"
+		sed -i -E "$wlanLine,$ s/^(\s*static routers.*)/#\1/" /etc/dhcpcd.conf  # Comment-out "routers"
+		sed -i -E "$wlanLine,$ s/^(\s*static domain_name_servers.*)/#\1/" /etc/dhcpcd.conf  # Comment-out "domain_name_servers"
+		sed -i -E "s/^#(\s*nohook wpa_supplicant.*)/\1/" /etc/dhcpcd.conf  #Un-comment "nohook wpa_supplicant"
 		#Read the current value:
 		oldPiIpV4=$(sed -n -E 's|^\s*static ip_address=(([0-9]{1,3}\.){3}[0-9]{1,3})/([0-9]{1,2}).*$|\1|p' /etc/dhcpcd.conf | tail -1) # Delimiter needs to be '|'
 		#echo $oldPiIpV4
@@ -272,21 +285,20 @@ END
 	fi
 	if [ -z "$oldPiIpV4" ]; then oldPiIpV4='10.10.10.1'; fi
 
-	if  grep -Fq "interface=wlan0" "/etc/dnsmasq.conf";
+	if  grep -q "interface=wlan0" /etc/dnsmasq.conf;
 	then
 		#Read the current values:
-		# This matches the format of the DHCP syntax:
+		wlanLine=$(sed -n '/interface=wlan0/=' /etc/dnsmasq.conf) #This is the line number that the wlan config starts at
+		oldDhcpStartIp=$(sed -n -E "$wlanLine,$ s|^\s*dhcp-range=(.*)$|\1|p" /etc/dnsmasq.conf ) # Delimiter is '|'
 		matchRegex="\s*(([0-9]{1,3}\.){3}[0-9]{1,3}),(([0-9]{1,3}\.){3}[0-9]{1,3}),(([0-9]{1,3}\.){3}[0-9]{1,3})," # Bash doesn't do digits as "\d"
-		while read line; do
-			if [[ $line =~ $matchRegex ]] ;
+		if [[ $oldDhcpStartIp =~ $matchRegex ]] ;
 			then
 				oldDhcpStartIp=${BASH_REMATCH[1]}
 				oldDhcpEndIp=${BASH_REMATCH[3]}
 				oldDhcpSubnetMask=${BASH_REMATCH[5]}
-				break
 			fi
-		done </etc/dnsmasq.conf
 	else
+		echo "No IPs in /etc/dnsmasq.conf. Adding some defaults"
 		#Create default values:
 		cat <<END >> /etc/dnsmasq.conf
 interface=wlan0      # Use the required wireless interface - usually wlan0
@@ -340,21 +352,51 @@ END
 
 unmake_ap ()
 {
-	systemctl mask hostapd
-	systemctl stop dnsmasq
+	systemctl disable dnsmasq #Stops it launching on bootup
+	systemctl disable hostapd
+	systemctl stop dnsmasq #Stops it NOW
 	systemctl stop hostapd
 	sed -i -E "s|^\s*(DAEMON_CONF=\")(.*)\"|#\1/etc/hostapd/hostapd.conf\"|" /etc/default/hostapd
-	if grep -qi "interface wlan0" /etc/dhcpcd.conf;
-	then
-		# Comment out the wlan0 lines:
-		sed -i -E "s/^(interface wlan0\s*)/#\1/" /etc/dhcpcd.conf
-		# https://unix.stackexchange.com/questions/285160/how-to-edit-next-line-after-pattern-using-sed
-		sed -i -E '$!N;s/(wlan0\n)(\s*static)/\1#\2/;P;D' /etc/dhcpcd.conf # Replaces only if "static" is on the line AFTER "wlan0"
-		sed -i -E "s/^(\s*nohook wpa_supplicant.*)/#\1/" /etc/dhcpcd.conf
-	else
-		echo -e "Skipped: interface wlan0 is not specified in /etc/dhcpcd.conf"
-	fi
 
+	wlanLine=$(sed -n '/interface wlan0/=' /etc/dhcpcd.conf) #This is the line number that the wlan config starts at
+	echo ""
+	read -p "Do you want to assign the Pi a static IP address? [Y/n]: " staticResponse
+	case $staticResponse in
+		(y|Y|"")
+			oldPiIpV4=$(sed -n -E 's|^#?\s*static ip_address=(([0-9]{1,3}\.){3}[0-9]{1,3})/([0-9]{1,2}).*$|\1|p' /etc/dhcpcd.conf | tail -1) # Delimiter needs to be '|'
+			oldDhcpSubnetCIDR=$(sed -n -E 's|^#?\s*static ip_address=(([0-9]{1,3}\.){3}[0-9]{1,3})/([0-9]{1,2}).*$|\3|p' /etc/dhcpcd.conf | tail -1) # Delimiter needs to be '|'
+			oldRouter=$(sed -n -E 's|^#?\s*static routers=(([0-9]{1,3}\.){3}[0-9]{1,3}).*$|\1|p' /etc/dhcpcd.conf | tail -1) # Delimiter needs to be '|'
+			oldDnsServers=$(sed -n -E 's|^#?\s*static domain_name_servers=(.*)$|\1|p' /etc/dhcpcd.conf | tail -1) # Delimiter needs to be '|'
+			oldDhcpSubnetMask=$(CIDRtoNetmask $oldDhcpSubnetCIDR)
+			read -e -i "$oldPiIpV4" -p         "Choose an IP address for the Pi         : " piIpV4
+			read -e -i "$oldDhcpSubnetMask" -p "Set the appropriate subnet mask         : " dhcpSubnetMask
+			read -e -i "$oldRouter" -p         "Set the Router IP                       : " router
+			read -e -i "$oldDnsServers" -p     "Set the DNS Server(s) (space-delimited) : " DnsServers
+			
+			cidr_mask=$(IPprefix_by_netmask $dhcpSubnetMask)
+			#Paste in the new settings
+			sed -i -E "s/^#(interface wlan0.*)/\1/" /etc/dhcpcd.conf
+			sed -i -E "$wlanLine,$ s|^#?(\s*static ip_address=)(.*)|\1$piIpV4/$cidr_mask|" /etc/dhcpcd.conf #Used "|" as the delimiter, as "/" is in the replacement string
+			sed -i -E "$wlanLine,$ s|^#?(\s*static routers=)(.*)|\1$router|" /etc/dhcpcd.conf #Used "|" as the delimiter, as "/" is in the replacement string
+			sed -i -E "$wlanLine,$ s|^#?(\s*static domain_name_servers=)(.*)|\1$DnsServers|" /etc/dhcpcd.conf #Used "|" as the delimiter, as "/" is in the replacement string
+			sed -i -E "s/^(\s*nohook wpa_supplicant.*)/#\1/" /etc/dhcpcd.conf  # Comment-out "nohook wpa_supplicant", as this line prevents us trying to connect to a WiFi network
+			;;
+		(*)
+			if grep -qi "interface wlan0" /etc/dhcpcd.conf;
+			then
+				# Comment out the wlan0 lines:
+				sed -i -E "s/^(interface wlan0\s*)/#\1/" /etc/dhcpcd.conf
+				# https://unix.stackexchange.com/questions/285160/how-to-edit-next-line-after-pattern-using-sed
+				sed -i -E '$!N;s/(wlan0\n)(\s*static\s*ip_address)/\1#\2/;P;D' /etc/dhcpcd.conf # Replaces only if "static ip_address" is on the line AFTER "wlan0"
+				sed -i -E "$wlanLine,$ s/^(\s*static routers.*)/#\1/" /etc/dhcpcd.conf  # Comment-out "routers"
+				sed -i -E "$wlanLine,$ s/^(\s*static domain_name_servers.*)/#\1/" /etc/dhcpcd.conf  # Comment-out "domain_name_servers"
+				sed -i -E "s/^(\s*nohook wpa_supplicant.*)/#\1/" /etc/dhcpcd.conf  # Comment-out "nohook wpa_supplicant", as this line prevents us trying to connect to a WiFi network
+			else
+				echo -e "Skipped: interface wlan0 is not specified in /etc/dhcpcd.conf"
+			fi
+			;;
+	esac
+	
 	echo "WARNING: After the next reboot, the Pi will come up as a WiFi *client*"
 	ssid=$(sed -n -E 's/^\s*ssid="(.*)"/\1/p' /etc/wpa_supplicant/wpa_supplicant.conf)
 	echo -e "WARNING: It will attempt to connect to this/these SSIDs:\n$ssid"
