232456e6761ec4faccb7e61ccd7edb0b7f5511f6
Greig Sheridan <greiginsydney@users.noreply.github.com>
2019-10-08 21:03:05 +1100

    Updated to same structure as piTransfer.py
    - Now reads INI file before deciding if it's going to copy or not
    - INI can be overridden with "copyNow" switch from the command line

--- Raspberry Pi/www/cameraTransfer.py
+++ Raspberry Pi/www/cameraTransfer.py
@@ -19,18 +19,61 @@
 # A pre-req task is that the Arduino has woken the camera, which we assume has taken place by the time the script runs.
 
 from urllib2 import urlopen, URLError
+import ConfigParser # for the ini file
+import datetime
 import logging
 import os
-from datetime import datetime
+import sys
+import time
+
+# ////////////////////////////////
+# /////////// STATICS ////////////
+# ////////////////////////////////
 
 LOGFILE_PATH = os.path.expanduser('/home/pi')
 LOGFILE_NAME = os.path.join(LOGFILE_PATH, 'cameraTransfer.log')
+INIFILE_PATH = os.path.expanduser('/home/pi')
+INIFILE_NAME = os.path.join(INIFILE_PATH, 'www/intvlm8r.ini')
 
 htmltext = ''
 
 
-def main():
+def main(argv):
     logging.basicConfig(filename=LOGFILE_NAME, filemode='w', format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S', level=logging.DEBUG)
+    copyNow = False
+    try:
+        if sys.argv[1] == 'copyNow':
+            copyNow = True
+    except:
+        pass
+
+    if not os.path.exists(INIFILE_NAME):
+        pass
+    config = ConfigParser.SafeConfigParser(
+        {
+        'copyDay'         : 'Off',
+        'copyHour'        : '',
+        })
+    config.read(INIFILE_NAME)
+    try:
+        copyDay       = config.get('Copy', 'copyDay')
+        copyHour      = config.get('Copy', 'copyHour')
+
+    except Exception as e:
+        copyDay = 'Off' # If we hit an exception, force copyDay=Off
+        log('INI file error:' + str(e))
+
+    now = datetime.datetime.now()
+    if (((now.strftime("%A") == copyDay) | (copyDay == "Daily")) & (now.strftime("%H") == copyHour)):
+        # We're OK to copy now
+        log('OK to copy @ %s:00.' % (copyHour))
+    elif copyNow == True:
+        # We're OK to copy now
+        log("OK to copy on 'copyNow'.")
+    else:
+        log('Not OK to copy.')
+        return
+        
     try:
         response = urlopen('http://localhost/transfer?copyNow=1')
         log('Response code = ' + str(response.getcode()))
@@ -57,4 +100,4 @@ def log(message):
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
