1088a5235ff097d798437a93a3aeead32f534b33
Greig Sheridan <greiginsydney@users.noreply.github.com>
2020-07-18 17:14:06 +1000

    import pyexiv2

--- Raspberry Pi/www/intvlm8r.py
+++ Raspberry Pi/www/intvlm8r.py
@@ -29,6 +29,7 @@ import io   #Camera preview
 import logging
 import os
 import psutil
+import pyexiv2 #Thumb conversions
 import re    #RegEx. Used in Copy Files
 from smbus2 import SMBus # For I2C
 import struct
