8be78cb4d137f908bf05f0eaccbf288e577ec8ea
Greig Sheridan <greiginsydney@users.noreply.github.com>
2021-03-21 14:27:17 +1100

    Added 'midnight' cmdline option
    This script runs under a cron job at midnight, but aborts if the Pi is using NTP as its clock source.
    
    Partly addresses Issue #51

--- Raspberry Pi/www/setTime.py
+++ Raspberry Pi/www/setTime.py
@@ -39,6 +39,26 @@ newTime = 'Unknown'
 def main(argv):
     logging.basicConfig(filename=LOGFILE_NAME, filemode='a', format='{asctime} {message}', style='{', datefmt='%Y/%m/%d %H:%M:%S', level=logging.DEBUG)
     log('')
+    # If the 'midnight' flag is set, do NOT run if the systemd-timesyncd is active (as this means NTP controls the Pi's RTC)
+    if sys.argv[1] == 'midnight':
+        try:
+            cmd = ['systemctl', 'is-active', 'systemd-timesyncd']
+            result = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False, encoding='utf-8')
+            (stdoutdata, stderrdata) = result.communicate()
+            if stdoutdata:
+                stdoutdata = stdoutdata.strip()
+                if stdoutdata == 'active':
+                    log('systemd-timesyncd = ' + str(stdoutdata) + '. The Pi takes its time from NTP. setTime.py aborting')
+                    return
+                else:
+                    log('systemd-timesyncd = ' + str(stdoutdata) + '. The Pi does NOT take its time from NTP. setTime.py continuing')
+            if stderrdata:
+                stderrdata = stderrdata.strip()
+                log('systemd-timesyncd error = ' + str(stderrdata) + '. setTime.py aborting')
+                return
+        except Exception as e:
+            log('Unhandled systemd-timesyncd error: ' + str(e) + '. setTime.py aborting')
+            return
     retryCount = 0
     while True:
         try:
