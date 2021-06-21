b528cefdcdb6685a0ece1a2cba1a07519511801f
Greig Sheridan <greiginsydney@users.noreply.github.com>
2021-06-22 08:50:21 +1000

    Added initial install steps for rawpy

--- Raspberry Pi/setup.sh
+++ Raspberry Pi/setup.sh
@@ -126,8 +126,25 @@ install_apps ()
 	#If the above doesn't install or throws errors, run apt-cache search libgphoto2 & it should reveal the name of the "development" version, which you should substitute back into your repeat attempt at this step.
 	pip3 install -v gphoto2
 	apt-get install libjpeg-dev libopenjp2-7 -y
-	pip3 install -v pillow --no-cache-dir
+	#pip3 install -v pillow --no-cache-dir
 	pip3 install ExifReader
+	
+	#Initial install steps for rawpy:
+	apt install git -y
+	apt-get install cmake -y
+	git clone https://github.com/LibRaw/LibRaw.git libraw
+	git clone https://github.com/LibRaw/LibRaw-cmake.git libraw-cmake
+	cd libraw
+	git checkout 0.20.0
+	cp -R ../libraw-cmake/* .
+	cmake .
+	sudo make install
+
+	git clone https://github.com/letmaik/rawpy
+	cd rawpy
+	pip3 install numpy cython
+	pip3 install .
+	rm -r LibRaw-master -f
 	
 	pip3 install smbus2
 	apt-get install i2c-tools -y
