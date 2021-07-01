greiginsydney/Intervalometerator

commit ca386f684b9eff10c2d295f738ea5ab1537853ed

Moved 'thisImage' to only now increment AFTER copy success

Raspberry Pi/www/intvlm8r.py
@@ -1749,11 +1749,12 @@ def copyNow(self):
         deleteAfterCopy = getIni('Copy', 'deleteAfterCopy', 'bool', 'Off')
         while len(filesToCopy) > 0:
             try:
-                thisImage += 1
                 self.update_state(state='PROGRESS', meta={'status': 'Copying image ' + str(thisImage) + ' of ' + str(numberToCopy)})
                 thisFile = filesToCopy.pop(0)
                 app.logger.info('About to copy file: ' + str(thisFile))
-                copy_files(camera, thisFile, deleteAfterCopy)
+                copyResult = copy_files(camera, thisFile, deleteAfterCopy)
+                if copyResult == 0:
+                    thisImage += 1
             except Exception as e:
                 app.logger.info('Unknown error in copyNow(): ' + str(e))
     try:

commit 3e450ec7202dc7d596adc6b734ed9b2d6e619e37

Updated on-screen count to allow for zero-reference
(Resolves bug introduced in commit [ca386f6](https://github.com/greiginsydney/Intervalometerator/commit/ca386f684b9eff10c2d295f738ea5ab1537853ed)

Raspberry Pi/www/intvlm8r.py
@@ -1759,7 +1759,7 @@ def copyNow(self):
         deleteAfterCopy = getIni('Copy', 'deleteAfterCopy', 'bool', 'Off')
         while len(filesToCopy) > 0:
             try:
-                self.update_state(state='PROGRESS', meta={'status': 'Copying image ' + str(thisImage) + ' of ' + str(numberToCopy)})
+                self.update_state(state='PROGRESS', meta={'status': 'Copying image ' + str(thisImage + 1) + ' of ' + str(numberToCopy)})
                 thisFile = filesToCopy.pop(0)
                 app.logger.info('About to copy file: ' + str(thisFile))
                 copyResult = copy_files(camera, thisFile, deleteAfterCopy)