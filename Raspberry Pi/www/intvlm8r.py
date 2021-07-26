ed701fe15e6efc5d1b5aeea8ed580f6a428c921a
Greig Sheridan <greiginsydney@users.noreply.github.com>
2021-07-26 14:20:47 +1000

    Replaced SimpleCache with Redis cache
    Should fix Issue #11

--- Raspberry Pi/www/intvlm8r.py
+++ Raspberry Pi/www/intvlm8r.py
@@ -43,9 +43,6 @@ import time
 
 import gphoto2 as gp
 
-from cachelib import SimpleCache
-cache = SimpleCache()
-
 from werkzeug.security import check_password_hash
 
 from flask import Flask, flash, render_template, request, redirect, url_for, make_response, abort, jsonify, g
@@ -63,6 +60,8 @@ app.jinja_env.trim_blocks = True
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['result_backend'])
 celery.conf.update(app.config)
+
+cache = Cache(app, config={'CACHE_TYPE': 'redis', 'CACHE_REDIS_URL': 'redis://localhost:6379/0'})
 
 login_manager = LoginManager()
 login_manager.init_app(app)
