49e163f0ed816b54c0cafe0975a769d3183ecda4
Greig Sheridan <greiginsydney@users.noreply.github.com>
2021-09-01 11:48:19 +1000

    Reverted recent favicon code
    Deleted from here, reverted to in html.

--- Raspberry Pi/www/intvlm8r.py
+++ Raspberry Pi/www/intvlm8r.py
@@ -2324,12 +2324,6 @@ def getShotsPerDay():
         app.logger.debug('getShotsPerDay exception: ' + str(e))
         return None
 
-    
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-        'favicon.ico',mimetype='image/x-icon')
-
 
 @app.route('/robots.txt')
 def norobots():
