b7f3a31888863213dd2cefb152f4c6b0bb83f316
Greig Sheridan <greiginsydney@users.noreply.github.com>
2021-09-13 13:16:27 +1000

    Don't SLEEP if the readVbatteryFlag is set

--- Arduino/intervalometer.ino/intervalometer.ino
+++ Arduino/intervalometer.ino/intervalometer.ino
@@ -988,7 +988,7 @@ void loop()
   // - made any changes pushed via the Pi
   // .. so now provided the Pi isn't running and ALARM isn't still set, we can sleep!
 
-  if ((bitRead(PORTD, PI_POWER) == LOW) && (ALARM == false) && (resetArduinoFlag == false))
+  if ((bitRead(PORTD, PI_POWER) == LOW) && (ALARM == false) && (resetArduinoFlag == false) && (readVbatteryFlag == false))
   {
     // The Pi is powered-off. It's safe for us to sleep
     //Serial.println(" - About to sleep");
