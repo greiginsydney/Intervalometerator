12e99046cd82bef5707f5b0644a262d6ad61e3e2
Greig Sheridan <greiginsydney@users.noreply.github.com>
2021-09-15 08:29:54 +1000

    Clamped valid voltage readings

--- Arduino/intervalometer.ino/intervalometer.ino
+++ Arduino/intervalometer.ino/intervalometer.ino
@@ -646,6 +646,12 @@ void UpdateVoltage()
     float averageVolts = VoltageReading / 16;            // Average the 16 different readings
     VoltageReading = (int)((averageVolts * 183) / 1023); // Convert 0-1023 to 0-"180" (18.0) Volts. (We'll add the decimal in the Pi)
     VoltageReading += 10;                                // Add an offset to allow transfer as bytes. (Preventing 0v being NULL is the issue addressed here).
+    //Clamp valid voltages to a range of 0-18.0V (even though a reading anywhere near zero isn't possible)
+    if ((VoltageReading < 10) || (VoltageReading > 190))
+    {
+      VoltageReading = 10;
+      Serial.println( "YESSS!! Trapped an invalid voltage read at hour = " + String(thisHour));
+    }
     VoltageString[thisHour] = byte(VoltageReading);      // Insert this voltage reading in the array
     EEPROM.write(MEMVolt0 + thisHour, byte(VoltageReading));
     //Serial.println("Final Voltage read = " + String(VoltageReading) + " Volts");
