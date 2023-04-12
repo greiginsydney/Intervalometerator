cf8626261821c8cb42a067a3769e231374f108e9
Greig Sheridan <greiginsydney@users.noreply.github.com>
2023-04-12 13:14:56 +1000

    Temporarily removed new gpio-poweroff/gpio-led code
    Needs more testing before release, and I want to push a new version out in the meantime.
    See https://github.com/greiginsydney/Intervalometerator/commit/c31d7f7967614b6a48a85745c761b3f7378caa97 & https://github.com/greiginsydney/Intervalometerator/commit/7ddb37f74671254b610aafa86fedbab38ac3956b

--- Raspberry Pi/setup.sh
+++ Raspberry Pi/setup.sh
@@ -841,11 +841,13 @@ dtparam=act_led_activelow=on
 dtoverlay=gpio-shutdown,gpio_pin=17,active_low=1,gpio_pull=up
 
 #Set GPIO27 to follow the running state: it's High while running and 0 when shutdown is complete. The Arduino will monitor this pin.
-#dtoverlay=gpio-poweroff,gpiopin=27,active_low **LEGACY
-dtoverlay=gpio-led,gpio=27,trigger=default-on,active_low
+dtoverlay=gpio-poweroff,gpiopin=27,active_low #**LEGACY
+#dtoverlay=gpio-led,gpio=27,trigger=default-on,active_low - TEMPORARILY REMOVED 20230412 PENDING MORE TESTING
 END
 	fi
 
+: '
+TEMPORARILY REMOVED 20230412 PENDING MORE TESTING
 	if grep -q '^dtoverlay=gpio-poweroff' /boot/config.txt;
 	then
 		echo -e ""$YELLOW"'/boot/config.txt' contains legacy dtoverlay=gpio-poweroff. Updating.""$RESET"
@@ -853,7 +855,7 @@ END
 	else
 		echo "Skipped: '/boot/config.txt' does not contain legacy 'dtoverlay=gpio-poweroff'"
 	fi
-
+'
 	if [ -f www/intvlm8r.old ];
 	then
 		mv -fv www/intvlm8r.old www/intvlm8r.bak
