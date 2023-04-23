c79a00872e4b471aab3a0f3bf3a4a4eb5c9fac77
Greig Sheridan <greiginsydney@users.noreply.github.com>
2023-04-23 10:52:43 +1000

    Added rsynclog.txt in /PI_USER_HOME/

--- Raspberry Pi/www/piTransfer.py
+++ Raspberry Pi/www/piTransfer.py
@@ -718,7 +718,7 @@ def commenceRsync(rsyncUsername, rsyncHost, rsyncRemoteFolder):
             if not rsyncRemoteFolder.endswith('/'):
                 rsyncRemoteFolder += '/'
             destination = rsyncUsername + '@' + rsyncHost + ':' + rsyncRemoteFolder
-            cmd = ['/usr/bin/rsync', '-avz', '--rsh=/usr/bin/ssh', localPath, destination]
+            cmd = ['/usr/bin/rsync', '-avz', '--rsh=/usr/bin/ssh', '--log-file=' + PI_USER_HOME + '/rsynclog.txt', '--log-file-format=Copied %f', localPath, destination]
             result = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False, encoding='utf-8')
             (stdoutdata, stderrdata) = result.communicate()
             if stdoutdata:
