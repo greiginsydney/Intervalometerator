e9d6a91be170e30692c882fd0cb12ff058888eb1
Greig Sheridan <greiginsydney@users.noreply.github.com>
2023-05-10 10:27:00 +1000

    Added 10s loop

--- Raspberry Pi/myIp.sh
+++ Raspberry Pi/myIp.sh
@@ -1,8 +1,17 @@
 #!/bin/sh -e
 
-IP=$(hostname -I) || true
-if [ "$IP" ]; then
-        echo "\nMy IP address is $IP\n"
-else
-        echo "\nSorry, no IP address yet\n"
-fi
+# Thank you kaanw on the Pi forums for this one:
+# https://forums.raspberrypi.com/viewtopic.php?t=122145&sid=f81d3c032658e2961f4ad3f85cb32f91&start=25
+
+for i in 'seq 1 10';
+do
+        IP=$(hostname -I) || true
+        if [ "$IP" ];
+        then
+                echo "\nMy IP address is $IP\n"
+                break
+        else
+                echo "\nSorry, no IP address yet\n"
+                /bin/sleep 2
+        fi
+done
