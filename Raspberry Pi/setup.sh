5d34eb1a3c486e8d63cbfdb9cf934782ed0c3f63
Greig Sheridan <greiginsydney@users.noreply.github.com>
2024-01-08 07:48:20 +1000

    comment-out redis' sleep line

--- Raspberry Pi/setup.sh
+++ Raspberry Pi/setup.sh
@@ -959,6 +959,7 @@ install_website ()
 		done
   		sed -i -E "s|^(\s*ExecStart\s*=\s*)(/usr/local/bin/gunicorn)|\1/home/$SUDO_USER/venv/bin/gunicorn|g" /etc/systemd/system/intvlm8r.service 
 		sed -i -E "s|^(\s*CELERY_BIN\s*=\s*)(\"/usr/local/bin/celery\")|\1\"/home/$SUDO_USER/venv/bin/celery\"|g" /etc/default/celery
+  		sed -i -E "s|^(\s*ExecStartPost\s*=\s*)(.*)(sleep)(.*)$|\1\n# \2\3\4|g" /etc/systemd/system/redis.service		# Comment-out the ExecStartPost 'sleep' command
 	fi
 
 	# Check and reload services if required:
